bf8d60308566f910cf8249aa8313ae616086aab1
Kajal Mishra <mishrakajal207@gmail.com>
2026-03-15 23:51:10 +0530

    draw.io uml diagram added

--- Inventory_Manager/product.py
+++ Inventory_Manager/product.py
@@ -1,13 +1,14 @@
 class Product:
-    def __init__(self, name, price, daily_demand=1):
+    def __init__(self, name, price, daily_demand=1, fill_strategy=None, fill_interval_days=1):
         self.name = name
         self.price = price
-        self.daily_demand = daily_demand  # kitne units per day bikte hain
-
+        self.daily_demand = daily_demand
+        self.fill_strategy = fill_strategy
+        self.fill_interval_days = fill_interval_days
 
 class Factory:
     @staticmethod
-    def create_product(name, price, daily_demand=1):
-        return Product(name, price, daily_demand)
-    
+    def create_product(name, price, daily_demand=1, fill_strategy=None, fill_interval_days=1):
+        return Product(name, price, daily_demand, fill_strategy, fill_interval_days)
+
 
